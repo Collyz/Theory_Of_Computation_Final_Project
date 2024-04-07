9f016af5c8cc03d3f30afb6ebce961a8e4954861
Collyz <m.mowla03151@gmail.com>
2024-04-07 17:52:07 -0400

    Features added: highlighting, dragging, deleting. Started lines

--- scripts/index.js
+++ scripts/index.js
@@ -1,103 +1,153 @@
+// Holds all circles (circle data)
 let circles = [];
+// Holds all line data()
+let lines = [];
+// Previously selected 
+let selected = null;
+// Previous (curr blue) blue circle
+let lastBlue = null;
+// Dragging 
+let dragging = false;
+// Line Drawing
+let prevCirc = null;
+let currCirc = null;
 
-const s1 = ( sketch ) => {
-    let x = 100;
-    let y = 100;
-    sketch.setup = () => {
-        sketch.createCanvas(600, 600);
-      };
-    
-    sketch.draw = () => {
-        sketch.clear();
-        sketch.background(200);
-        for(let i = 0; i < circles.length; i++){
-            circles[i].display();
-        }
-    };
-    sketch.mousePressed = () => {
-        if(sketch.mouseButton === sketch.LEFT && true){
-            circles.push(new Circ(sketch.mouseX, sketch.mouseY));
-        }
-        if(sketch.mouseButton === sketch.CENTER){
-            for(let i = circles.length - 1; i >= 0; i--){
-                if(sketch.dist(sketch.mouseX, sketch.mouseY, circles[i].x, circles[i].y) < circles[i].r){
-                    circles.splice(i, 1);
-                    break;
-                }
+// P5 canvas setup
+function setup() {
+    let myCanvas = createCanvas(600, 400);
+    myCanvas.parent('canvasContainer');
+    background(200);
+}
+
+// Draws every frame
+function draw(){
+    clear();
+    background(200);
+
+    // Drawing Circles
+    for(let i = 0; i < circles.length; i++){
+        circles[i].display();
+    }
+
+    // Draw Lines
+    for(let i = 0; i < lines.length; i++){
+        lines[i].display();
+    }
+
+    // Updating position of selected blue circle when dragging
+    if(dragging && lastBlue !== null){
+        circles.splice(lastBlue, 1, new Circ(mouseX, mouseY, 'blue'));
+    }
+}
+
+// On single mouse click
+function mousePressed(){
+    // Selection loop
+    for(let i = 0 ; i < circles.length; i++){
+        if(dist(mouseX, mouseY, circles[i].x, circles[i].y) < circles[i].r){
+            // If there is a previous blue circle, check, if not, make current selection blue
+            if(lastBlue === null){
+                let tempX = circles[i].x;
+                let tempY = circles[i].y;
+                circles.splice(i, 1, new Circ(tempX, tempY, 'blue'));
+                lastBlue = i;
+                currCirc = i;
+            }else{
+                // If there is a previous blue circle, replace the old one 
+                let tempX = circles[lastBlue].x;
+                let tempY = circles[lastBlue].y;
+                circles.splice(lastBlue, 1, new Circ(tempX, tempY, 'None'));
+                // Make the new selected circle blue
+                let tempX2 = circles[i].x;
+                let tempY2 = circles[i].y;
+                circles.splice(i, 1, new Circ(tempX2, tempY2, 'blue'));
+                prevCirc = lastBlue;
+                lastBlue = i;
+                currCirc = i;
             }
-        }
-    };
-
-    class Circ {
-        constructor(x ,y) {
-            this.x = x;
-            this.y = y;
-            this.d = 30;
-            this.r = this.d / 2;
-    
-        }
-        display(){
-            sketch.circle(this.x, this.y, this.r * 2);
-        }
-    
-        x(){
-            return this.x;
+            
+            break;
         }
     }
-    
-};
+    // Dragging check
+    if(lastBlue !== null ** mousePressed == LEFT){
+        if(dist(mouseX, mouseY, circles[lastBlue].x, circles[lastBlue].y) < circles[lastBlue].r){
+            dragging = true;
+        }
+    }
+    console.log(lines.length);
+}
 
-// function mousePressed(){
-//     console.log("click")
-//     if(mouseButton === LEFT && true){
-//         circles.push(new Circ());
-//     }
-//     if(mouseButton === CENTER){
-//         for(let i = circles.length - 1; i >= 0; i--){
-//             if(dist(mouseX, mouseY, circles[i].x, circles[i].y) < circles[i].r){
-//                 circles.splice(i, 1);
-//                 break;
-//             }
-//         }
-//     }
-// }
+// On mouse released
+function mouseReleased(){
+    dragging = false;
+}
 
-let myp5 = new p5(s1, document.getElementById('canvasContainer'));
+// Delete blue circles
+window.addEventListener('keydown', function(e){
+    if(e.key === 'Delete'){
+        if(lastBlue !== null){
+            circles.splice(lastBlue, 1);
+            lastBlue = null;
+        }
+    }
 
+});
 
-// function setup() {
-//     let myCanvas = createCanvas(600, 400);
-//     myCanvas.parent('canvasContainer');
-//     // drawingContext.shadowOffsetX = 5;
-//     // drawingContext.shadowOffsetY = -5;
-//     // drawingContext.shadowBlur = 6;
-//     // drawingContext.shadowColor = "black";
-//     background(200);
-// }
-
-
-// function draw(){
-//     clear();
-//     background(200);
-//     for(let i = 0; i < circles.length; i++){
-//         circles[i].display();
-//     }
-// }
-
+// Draw Lines????
+window.addEventListener("click", function(e){
+    if(e.shiftKey){
+        // for(let i = circles.length - 1; i >= 0; i--){
+        //     if(dist(mouseX, mouseY, circles[i].x, circles[i].y) < circles[i].r){
+        //         if(circles[i].color === 'blue'){
+        //             lastBlue = null;
+        //         }
+        //         circles.splice(i, 1);
+        //         break;
+        //     }
+        // }
+        dragging = false;
+        if(lastBlue !== null){
+            if(currCirc !== null && prevCirc !== null){
+                lines.push(new Arrow(circles[prevCirc].x, circles[prevCirc].y, circles[currCirc].x, circles[currCirc].y));
+            }
+        }
+    }
+});
 
 // Behavior: Triggers only once per click, i.e. cannot drag to create
 
+// On Double Click, draw a circle
+
+ondblclick = (event) =>{
+    if(mouseButton === LEFT){
+        circles.push(new Circ(null, null,'None'));
+    }
+};
+
+
 
 class Circ {
-    constructor(x ,y) {
-        this.x = x;
-        this.y = y;
+    constructor(x, y, color){
+        if(x === null && y === null){
+            this.x = mouseX;
+            this.y = mouseY;
+        } else{
+            this.x = x;
+            this.y = y;
+        }
         this.d = 30;
-        this.r = this.d / 2;
-
+        this.r = this.d/2;
+        this.color = color;
     }
+
     display(){
-        sketch.circle(this.x, this.y, this.r * 2);
+        if(this.color !== 'None'){
+            stroke(this.color)
+        }else{
+            stroke(0);
+        }
+        circle(this.x, this.y, this.r * 2);
     }
 
     x(){
@@ -105,11 +155,16 @@ class Circ {
     }
 }
 
-class Line {
-    constructor(){
-        this.x1 = mouseX;
-        this.y1 = mouseY;
-        this.x2 = mouseX;
-        this.x2 = mouseY;
+class Arrow {
+    constructor(x1, y1, x2, y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        console.log(this.x1 + " " + this.y1 + " " + this.x2 + " " + this.y2);
+    }
+
+    display(){
+        line(this.x1, this.y1, this.x2, this.y2);
     }
 }
